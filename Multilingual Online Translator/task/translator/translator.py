899cc9242a94783b07b174a7db8156927f432656
fvn70 <fvn_reg@mail.ru>
2022-02-17 11:55:30 +0700

    new Stage 5/7

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -14,41 +14,46 @@ def do_request(k_from, k_to, word):
         if tr_content:
             lst = tr_content.find_all('a', class_=re.compile('translation.*'))
             if lst:
-                for l in lst:
-                    words.append(l.text.strip())
+                words[k_to] = lst[0].text.strip()
 
                 ex_content = soup.find('section', {'id':"examples-content"})
                 lst = ex_content.find_all('span', {'class':'text'})
-                for l in lst:
-                    examples.append(l.text.strip())
-
-                print_result(k_to)
+                examples[k_to] = [lst[0].text.strip(), lst[1].text.strip()]
+            print_result(k_to)
     else:
         print(r.status_code)
 
 
 def print_result(k):
-    print(f"\n{langs[k]} Translations:")
-    for word in words[:5]:
-        print(word)
-    print(f"\n{langs[k]} Examples:")
-    for i in range(0, 10, 2):
-        print(examples[i])
-        print(examples[i + 1])
-        print()
+    print(f"\n{langs[k]} Translations:\n{words[k]}")
+    print(f"\n{langs[k]} Examples:\n{examples[k][0]}\n{examples[k][1]}")
+
+
+def write_results():
+    with open(fn, 'wb') as file:
+        for k in words:
+            file.write(bytes(f"\n{langs[k]} Translations:\n{words[k]}\n", encoding='utf-8'))
+            file.write(bytes(f"\n{langs[k]} Examples:\n{examples[k][0]}\n{examples[k][1]}\n", encoding='utf-8'))
 
 
 base = "https://context.reverso.net/translation"
 langs = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese',
          'Dutch', 'Polish', 'Portuguese', 'Romanian', 'Russian', 'Turkish']
-words = []
-examples = []
+words = {}
+examples = {}
 
 print("Hello, you're welcome to the translator. Translator supports:")
 for i in range(len(langs)):
     print(f"{i + 1}. {langs[i]}")
-lng_from = input('Type the number of your language:\n')
-lng_to = input('Type the number of language you want to translate to:\n')
+lng_from = int(input('Type the number of your language:\n')) - 1
+lng_to = int(input('Type the number of language you want to translate to:\n')) - 1
 word = input('Type the word you want to translate:\n')
+fn = f'{word}.txt'
 
-do_request(int(lng_from) - 1, int(lng_to) - 1, word)
+if lng_to < 0:
+    for k in range(len(langs)):
+        if k != lng_from:
+            do_request(lng_from, k, word)
+else:
+    do_request(lng_from, lng_to, word)
+write_results()
